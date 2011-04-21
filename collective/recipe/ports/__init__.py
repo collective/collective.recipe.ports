d61dc7b83ef05b53d63417bc58a5cafe81aab0d2
Alex Clark <aclark@aclark.net>
2011-04-20 22:53:59 -0400

    Write defaults to ports.cfg

--- collective/recipe/ports/__init__.py
+++ collective/recipe/ports/__init__.py
@@ -1,4 +1,10 @@
+import ConfigParser
 import os
+
+default = """[ports]
+instance1 = 8080
+zeo = 8100
+"""
 
 class Recipe(object):
 
@@ -6,12 +12,16 @@ class Recipe(object):
         self.buildout, self.name, self.options = buildout, name, options
 
     def install(self):
+
         # If there is no ports.cfg file specified, create one in the current working dir.
+
         if not 'config' in self.options:
             filename = os.path.join(self.buildout['buildout']['directory'], 'ports.cfg')
             config = open(filename, 'w')
+            config.write(default)
             config.close()
 
+        # Now use configparser to read in the ports
         return tuple()
 
     def update(self):
