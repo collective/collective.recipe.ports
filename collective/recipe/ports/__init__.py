e30b23eb91de53ed9f06e0e725dcd97896e3c85c
Alex Clark <aclark@aclark.net>
2011-04-20 22:42:36 -0400

    Make it write an empty file

--- collective/recipe/ports/__init__.py
+++ collective/recipe/ports/__init__.py
@@ -1,3 +1,4 @@
+import os
 
 class Recipe(object):
 
@@ -5,6 +6,12 @@ class Recipe(object):
         self.buildout, self.name, self.options = buildout, name, options
 
     def install(self):
+        # If there is no ports.cfg file specified, create one in the current working dir.
+        if not 'config' in self.options:
+            filename = os.path.join(self.buildout['buildout']['directory'], 'ports.cfg')
+            config = open(filename, 'w')
+            config.close()
+
         return tuple()
 
     def update(self):
