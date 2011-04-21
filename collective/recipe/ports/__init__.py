d73609cd20de946dea9f2932edf7465d20e07c2b
Alex Clark <aclark@aclark.net>
2011-04-21 12:10:16 -0400

    Simplify

--- collective/recipe/ports/__init__.py
+++ collective/recipe/ports/__init__.py
@@ -3,18 +3,16 @@ import os
 
 default = """[ports]
 instance1 = 8080
-zeo = 8100
 """
 
 class Recipe(object):
 
     def __init__(self, buildout, name, options):
         self.buildout, self.name, self.options = buildout, name, options
-
-    def install(self):
+        
+        self.options['port'] = str(get_port())
 
         # If there is no ports.cfg file specified, create one in the current working dir.
-
         if not 'config' in self.options:
             filename = os.path.join(self.buildout['buildout']['directory'], 'ports.cfg')
             config = open(filename, 'w')
@@ -22,6 +20,16 @@ class Recipe(object):
             config.close()
 
         # Now use configparser to read in the ports
+        cp = ConfigParser.RawConfigParser()
+        cp.read(filename)
+
+        # At this point we can check cp.items() for in-use ports,
+        # so start assigning ports.
+        
+        
+            
+
+    def install(self):
         return tuple()
 
     def update(self):
